69722abc39a6084c4191bde92f7142a8bb778bfb
Jason Knoll <jason.p.knoll@gmail.com>
2021-08-08 16:04:40 -0700

    Add model test for discounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 7) do
+ActiveRecord::Schema.define(version: 2021_08_08_200723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "discounts", force: :cascade do |t|
+    t.float "percentage"
+    t.integer "threshold"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/models/discounts_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Discount do
+  describe 'relationships' do
+    it {should belong_to :merchant}
+  end
+
+  describe 'validations'
+
+end
