d74320b7dd1a964f6a840e5d9572ca000867d176
Jason Knoll <jason.p.knoll@gmail.com>
2021-08-08 21:04:13 -0700

    Add factorybot for discounts

---
+++ spec/factories/discount.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :discount do
+    # percentage { Faker::Number.between(from: 0.10, to: 0.50) }
+    # threshold { Faker::Number.between(from: 10, to: 50, increment: 5) }
+    percentage { [0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.45,0.50].sample}
+    threshold { [10,15,20,25,30,35,40,45,50].sample }
+
+
+  end
+end
+
+#sample
