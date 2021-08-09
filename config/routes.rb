483ee84714d9c9852e31b1d6e0c713d70c3a11d4
Jason Knoll <jason.p.knoll@gmail.com>
2021-08-08 22:09:56 -0700

    Add links for discount items show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :merchants, module: :merchant do
     # get '/dashboard', to: 'dashboard#show', as: 'merchant_dashboard'
-    resources :discounts, only: [:index]
+    resources :discounts, only: [:index, :show]
     resources :dashboard, only: [:index]
     resources :items
     resources :invoices
