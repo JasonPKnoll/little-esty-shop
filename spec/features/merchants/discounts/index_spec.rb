3f0a85ed6c1d50db8a26f9c88caa2bb058a81cbe
Jason Knoll <jason.p.knoll@gmail.com>
2021-08-09 04:17:27 -0700

    Add a create new discount form

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -14,36 +14,41 @@ RSpec.describe 'Merchant discounts index page' do
     it "I see all discounts for this merchant" do
       visit "merchants/#{@merchant_1.id}/discounts"
 
-      expect(page).to have_content(@discounts.first.percentage * 100)
+      expect(page).to have_content(@discounts.first.percentage)
       expect(page).to have_content(@discounts.first.threshold)
 
-      expect(page).to have_content(@discounts.second.percentage * 100)
+      expect(page).to have_content(@discounts.second.percentage)
       expect(page).to have_content(@discounts.second.threshold)
 
-      expect(page).to have_content(@discounts.third.percentage * 100)
+      expect(page).to have_content(@discounts.third.percentage)
       expect(page).to have_content(@discounts.third.threshold)
     end
 
     it "has a link for each discount to its show page" do
       visit "merchants/#{@merchant_1.id}/discounts"
+
       within "#discount-#{@discounts.first.id}" do
-        click_link "#{@discounts.first.percentage * 100}% Discount"
+        click_link "#{@discounts.first.percentage}% Discount"
       end
 
       expect(current_path).to eq("/merchants/#{@merchant_1.id}/discounts/#{@discounts.first.id}")
     end
 
     it "has a link to create a new discount" do
+      visit "merchants/#{@merchant_1.id}/discounts"
+
       click_link "Create Discount"
 
       expect(current_path).to eq("/merchants/#{@merchant_1.id}/discounts/new")
     end
 
     it "can create a new discount" do
+      visit "merchants/#{@merchant_1.id}/discounts"
+
       click_link "Create Discount"
 
-      fill_in :percentage, with: 90
-      fill_in :quantity, with: 100
+      fill_in :percentage, with: 90.5
+      fill_in :threshold, with: 100
 
       click_button "Submit"
 
@@ -51,6 +56,19 @@ RSpec.describe 'Merchant discounts index page' do
 
       expect(page).to have_content(90)
       expect(page).to have_content(100)
+    end
+
+    it "should flash success when creating a discount succesfully" do
+      visit "merchants/#{@merchant_1.id}/discounts"
+
+      click_link "Create Discount"
+
+      fill_in :percentage, with: 90.5
+      fill_in :threshold, with: 100
+
+      click_button "Submit"
+
+      expect(page).to have_content("Success: New discount has been created!")
     end
   end
 end
