5ed6e8dbbe9ed02137afffdfdf0e4b9e1ea3ea32
Jason Knoll <jason.p.knoll@gmail.com>
2021-08-08 22:09:35 -0700

    Add test for discount links from index to show page

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -14,14 +14,23 @@ RSpec.describe 'Merchant discounts index page' do
     it "I see all discounts for this merchant" do
       visit "merchants/#{@merchant_1.id}/discounts"
 
-      expect(page).to have_content(@discounts.first.percentage)
+      expect(page).to have_content(@discounts.first.percentage * 100)
       expect(page).to have_content(@discounts.first.threshold)
 
-      expect(page).to have_content(@discounts.second.percentage)
+      expect(page).to have_content(@discounts.second.percentage * 100)
       expect(page).to have_content(@discounts.second.threshold)
 
-      expect(page).to have_content(@discounts.third.percentage)
+      expect(page).to have_content(@discounts.third.percentage * 100)
       expect(page).to have_content(@discounts.third.threshold)
+    end
+
+    it "has a link for each discount to its show page" do
+      visit "merchants/#{@merchant_1.id}/discounts"
+      within "#discount-#{@discounts.first.id}" do
+        click_link "#{@discounts.first.percentage * 100}% Discount"
+      end
+
+      expect(current_path).to eq("/merchants/#{@merchant_1.id}/discounts/#{@discounts.first.id}")
     end
   end
 end
